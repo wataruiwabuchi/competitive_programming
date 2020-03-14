b98bce2f8cb1869236c867f4f3ff52f36644537b
wataruiwabuchi <speeeedcubist@gmail.com>
2020-03-14 18:10:33 +0900

    素因数分解を追加

--- library.py
+++ library.py
@@ -78,3 +78,22 @@ def pow_k_mod(x, n, m):
         n //= 2
 
     return (K * x) % m
+
+
+# nを素因数分解
+# 各素因数を数え上げる場合はcounterを使うとよい
+def prime_factorize(n):
+    a = []
+    while n % 2 == 0:
+        a.append(2)
+        n //= 2
+    f = 3
+    while f * f <= n:
+        if n % f == 0:
+            a.append(f)
+            n //= f
+        else:
+            f += 2
+    if n != 1:
+        a.append(n)
+    return a
