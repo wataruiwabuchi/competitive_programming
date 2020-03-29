a040b309526ec626382294dfdd7b835d2f255eb9
wataruiwabuchi <speeeedcubist@gmail.com>
2020-03-29 18:17:12 +0900

    abc160_a

---
+++ atcoder/abc160_a.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    string S;
+    cin >> S;
+
+    if(S[2] == S[3] && S[4] == S[5])
+        cout << "Yes" << endl;
+    else
+        cout << "No" << endl;
+}
