217a274b0e17fd620557475623951cb01d86c12b
wataruiwabuchi <speeeedcubist@gmail.com>
2020-04-10 20:42:03 +0900

    add

---
+++ atcoder/abc161_a.cpp
@@ -0,0 +1,24 @@
+#define _USE_MATH_DEFINES
+#include <iostream>
+#include <string>
+#include <vector>
+#include <map>
+#include <set>
+#include <cmath>
+#include <algorithm>
+#include <iomanip>
+using namespace std;
+#define MOD int(1e9 + 7)
+
+int main()
+{
+    long long X, Y, Z;
+    cin >> X >> Y >> Z;
+
+    cout << Z;
+    cout << ' ';
+    cout << X;
+    cout << ' ';
+    cout << Y;
+
+}
