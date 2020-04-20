2b5150d3073557d35f29847b1bea42925fa1ddbd
wataruiwabuchi <speeeedcubist@gmail.com>
2020-04-20 22:11:44 +0900

    add modinv

--- library.cpp
+++ library.cpp
@@ -51,3 +51,15 @@ long long mod_pow(long long _x, long long _n)
     return ret;
 }
 
+// mod. m での a の逆元 a^{-1} を計算する
+long long modinv(long long a, long long m) {
+    long long b = m, u = 1, v = 0;
+    while (b) {
+        long long t = a / b;
+        a -= t * b; swap(a, b);
+        u -= t * v; swap(u, v);
+    }
+    u %= m;
+    if (u < 0) u += m;
+    return u;
+}
