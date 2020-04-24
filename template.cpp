5a4ab861117c7cadc27a1982eceb2ea7f883f6f9
wataruiwabuchi <speeeedcubist@gmail.com>
2020-04-24 18:21:22 +0900

    テンプレを作成

---
+++ template.cpp
@@ -0,0 +1,18 @@
+#define _USE_MATH_DEFINES
+#include <iostream>
+#include <string>
+#include <vector>
+#include <map>
+#include <set>
+#include <cmath>
+#include <algorithm>
+#include <iomanip>
+#include <climits>
+using namespace std;
+#define MOD int(1e9 + 7)
+#define INF (long long)(1e18)
+
+int main()
+{
+
+}
