654b84dfba7f877ee35a91e5f91716a901980d1d
wataruiwabuchi <speeeedcubist@gmail.com>
2020-04-27 22:52:10 +0900

    add

---
+++ atcoder/soundhound_2018_c.cpp
@@ -0,0 +1,25 @@
+#define _USE_MATH_DEFINES
+#include <iostream>
+#include <string>
+#include <vector>
+#include <map>
+#include <set>
+#include <queue>
+#include <stack>
+#include <cmath>
+#include <algorithm>
+#include <iomanip>
+#include <climits>
+using namespace std;
+#define MOD int(1e9 + 7)
+#define INF (long long)(1e18)
+
+int main()
+{
+    cout.precision(18);
+
+    long double N, M, D;
+    cin >> N >> M >> D;
+
+    cout << ((D) ? 2 : 1) * (M - 1) * (N - D) / (N * N) << endl;
+}
